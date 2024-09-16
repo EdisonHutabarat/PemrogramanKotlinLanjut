ac6d73a867ea4ad3c8d09b09c61b743ed0855bf0
EdisonHutabarat <edisonhutabaratp17@gmail.com>
2024-09-16 19:11:22 +0700

    Fungsi tanpa nilai kembali add FungsiLambdas1.kt

---
+++ src/main/kotlin/FungsiLambdas1.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    displayMessage("Rekayasa Perangkat Lunak", 10);
+}
+fun displayMessage(msg: String, count: Int) {
+    var counter = 1
+    while(counter <= count ) {
+        println("$msg $counter")
+        counter++
+    }
+}
