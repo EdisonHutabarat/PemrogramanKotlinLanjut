739dfce5dc9dbb5e27849f3211d6df792803c099
EdisonHutabarat <edisonhutabaratp17@gmail.com>
2024-09-16 18:28:20 +0700

    Penerapan For-loop dalam Kalkulator Eksponensial Add For-loop3.kt

---
+++ src/main/kotlin/For-loop3.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+    println("Exponent calculator")
+    println("===================")
+    println("Enter the base: ")
+    val a = readLine()!!.toInt()
+    println("Enter the exponent: ")
+    val n = readLine()!!.toInt()
+    var result = a
+    for (i in 1..n-1) {
+        result = result * a
+    }
+    println("Result: $result")
+    println("Thank you for using our exponent calculator")
+}
