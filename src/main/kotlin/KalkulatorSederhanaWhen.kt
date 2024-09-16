e0a7b7d7902af795fbd05603a3f534824b613142
EdisonHutabarat <edisonhutabaratp17@gmail.com>
2024-09-16 18:21:11 +0700

    Kalkulator sederhana menggunakan percabangan when KalkulatorSederhanaWhen.kt

---
+++ src/main/kotlin/KalkulatorSederhanaWhen.kt
@@ -0,0 +1,26 @@
+fun main(args: Array<String>) {
+    println("Welcome to our calculator")
+    println("Enter the first number:");
+    val a = readLine()!!.toDouble()
+    println("Enter the second number:")
+    val b = readLine()!!.toDouble()
+    println("Choose one of the following operations:")
+    println("1 - addition")
+    println("2 - subtraction")
+    println("3 - multiplication")
+    println("4 - division")
+    val choice = readLine()!!.toInt()
+    var result = 0.0
+    when (choice) {
+        1 -> result = a + b
+        2 -> result = a - b
+        3 -> result = a * b
+        4 -> result = a / b
+    }
+    if ((choice > 0) && (choice < 5)) {
+        println("Result: $result")
+    } else {
+        println("Invalid choice")
+    }
+    println("Thank you for using our calculator.")
+}
