95d7ad05a8f0766c432def962817b7e0be3155e7
EdisonHutabarat <edisonhutabaratp17@gmail.com>
2024-09-16 18:55:44 +0700

    program untuk pemeriksaan kondisi null PemeriksaanKondisiNull.kt

---
+++ src/main/kotlin/PemeriksaanKondisiNull.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    //Conditions
+    var maybeNumber: Int? = 15
+    if (maybeNumber != null)
+        println(maybeNumber * 2)
+    else
+        println("The entered value isn't a number")
+}
