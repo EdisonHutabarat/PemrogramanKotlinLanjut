0fd3ea62b81cad1998be61b1cbb16a996b6a756e
EdisonHutabarat <edisonhutabaratp17@gmail.com>
2024-09-16 18:45:57 +0700

    program pemberian nilai null yang diizinkan add NullSafety.kt

--- src/main/kotlin/NullSafety.kt
+++ src/main/kotlin/NullSafety.kt
@@ -5,13 +5,3 @@ fun main(args: Array<String>){
     println("maybeNumber: $maybeNumber ${maybeNumber?.hashCode()}")
 }
 
-// KESALAHAN PENERAPAN OPERATOR NULL
-//fun main(args: Array<String>) {
-//    var maybeNumber: Int? = 15
-//    maybeNumber = null
-//    println(maybeNumber * 2) --> terjadi kesalahan karena maybeNumber tidak menggunakan operator "?"
-//    var s1 = "Hello"
-//    var s2: String? = "World"
-//    println(s1.length)
-//    println(s2.length)
-//}
